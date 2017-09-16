a207acf503e13abc1acab9e04b681e1ff2e3284b
dkp5177@psu.edu <dkp5177@psu.edu>
2017-09-16 15:25:40 -0400

    second

--- PLS/pls.R
+++ PLS/pls.R
@@ -1,34 +1,19 @@
 require(lubridate)
-x = as.Date('2010-06-10')
-yday(x)
-
-
+require(pls)
+require(plsVarSel)
+require(ggplot2)
+require(scales)
 #####################################
 ###Partial least squares analysis###
 #####################################
+########################
+###Daily temperatures###
+########################
+###########################################################
+###Daily resolution - only looking at  September to April##
+###########################################################
 
-####Monthly resolution
-
-
-CM_CAST_ALL
-
-CM_month <- plsr(Julian~., data = CM_CAST_ALL[,2:10],validation = "LOO",scale=TRUE,
-                 method="oscorespls")
-summary(CM_month)
-
-test <- VIP(CM_month)
- barplot(colMeans(test),type='l')
- abline(h= 0.8)
-
-
-
-plot(CM_month, plottype='coef',ncomp= 1:3 ,legendpos = 'bottomleft',
-     lbaels='numbers',xlab='nm')
-
-oscorespls.fit(as.matrix(CM_CAST_ALL[,1]), as.matrix(CM_CAST_ALL[,2:10]),
-               ncomp=8)
-
-###Daily resolution - only looking at  September to April
+#Editing data 
 
 CM_TEMP_Daily <- subset(temp.main.dat2, year >1982 & year < 2017)
 row.names(CM_TEMP_Daily)<- seq(1, nrow(CM_TEMP_Daily))
@@ -36,25 +21,24 @@ CM_TEMP_Daily <- CM_TEMP_Daily[-(1:243),]
 row.names(CM_TEMP_Daily)<- seq(1, nrow(CM_TEMP_Daily))
 CM_TEMP_Daily <- CM_TEMP_Daily[-(11931:12175),]
 
+
+
+
+###Gettitng rid of 5,6,7,8
+#Getting rid of May to August
 CM_TEMP_Daily <- subset(CM_TEMP_Daily, CM_TEMP_Daily$month != 5 &
                           CM_TEMP_Daily$month != 6 &
                           CM_TEMP_Daily$month != 7&
                           CM_TEMP_Daily$month != 8)
 
-
-
-###Gettitng rid of 5,6,7,8
-
+#Figure out the julian day
 CM_TEMP_Daily$Julian <- yday(CM_TEMP_Daily$Date)
 
 
-
-
+#Only looking at tmin
 CM_TEMP_DAILY_SUB <- CM_TEMP_Daily[,c(8,11,5)]
 
 CM_CAST<-dcast(CM_TEMP_DAILY_SUB,year~ Julian)
-
-
 
 CM_CAST<-CM_CAST[,-367]
 
@@ -62,21 +46,107 @@ CM_CAST_ALL <- cbind(CM_CAST_ALL[,1], CM_CAST)
 
 CM.first.spring$julian
 
+###ACTUAL ANALYSIS OF CODLING MOTH (Julian day)
 ###
 CM_PLS <- read.csv("CM_MANUAL_JULIAN_PLS.csv",stringsAsFactors = FALSE)
 
-CM_month <- plsr(Julian~., data = CM_PLS[,2:244],validation = "LOO",scale=TRUE,
+
+###SMOOTHING IT OUT FOR EACH YEAR - EXAMPLE
+julian_day <-(c(244:365,1:120 ))
+
+JULIAN_DAY_TEMP_CM <- cbind.data.frame(colnames(CM_PLS[,3:244]),
+                                 t(CM_PLS[1,3:244]),
+                                as.vector(smooth(
+                                   as.vector(t(CM_PLS[1,3:244])),
+                                   twiceit=TRUE)))
+rownames(JULIAN_DAY_TEMP_CM) <- seq(1, length(julian_day))
+colnames(JULIAN_DAY_TEMP_CM)<-c('JD','tmin','smooth')
+
+
+ggplot(JULIAN_DAY_TEMP_CM , 
+       aes(x= JD, y= tmin,group=1))+
+  geom_point()+
+  geom_line()+
+  geom_point(aes(x= JD,y=smooth),col='red')+
+  geom_line(aes(x= JD,y=smooth),col='red')
+
+###SMOOTHING 
+
+Smoothed_df <-matrix(NA, nrow = 33, ncol = 242)
+
+for (i in seq(1,33)){
+  temp <- CM_PLS[i,]
+  Smoothed_df[i,]<-  as.vector(smooth(
+    as.vector(t(temp[,3:244])), twiceit=TRUE))
+  
+}
+
+SMOOTH_CM_data<-  cbind.data.frame(seq(1984,2016),
+                                   CM.first.spring$julian,
+                                   Smoothed_df)
+colnames(SMOOTH_CM_data)<- c("Year","Julian",
+                             colnames(CM_PLS[,3:244]))
+
+
+CM_month <- plsr(Julian~., data = SMOOTH_CM_data[,2:244],
+                 validation = "CV",scale=TRUE,
                  method="oscorespls")
 summary(CM_month)
 
-a<-VIP(CM_month)
-b<- data.frame(colnames(a),colMeans(a))
+VIP_CM_SMOOTH<-data.frame(julian_day,VIP(CM_month,opt.comp=1))
 
-b$realdate <- format(seq(as.Date("1984/9/1"), as.Date("1985/4/30"), "days"),'%m-%d')
-colnames(b) <- c('julian','vip','realdate')
-b$great <- ifelse(b$vip >= 0.8, TRUE,FALSE)
-ggplot(b, aes(x = realdate, y= vip, fill = great))+geom_bar(stat='identity')+
-  theme(axis.text.x = element_text(angle = 45, size = 9))
-  
-class(b$realdate)
+VIP_CM_SMOOTH$realdate <- (seq(as.Date("1984/9/1"),
+                               as.Date("1985/4/30"), "days"))
+colnames(VIP_CM_SMOOTH) <- c('julian','vip','realdate')
+VIP_CM_SMOOTH$great <- ifelse(VIP_CM_SMOOTH$vip >= 1, TRUE,FALSE)
+
+ggplot(VIP_CM_SMOOTH, 
+  aes(x = realdate, y= vip, fill = great))+geom_bar(stat='identity',
+  color='black')+
+  theme(axis.text.x = element_text(angle = 45, size = 9))+
+  theme(axis.ticks.length=unit(0.5,"cm"))+
+  scale_x_date(date_breaks = "2 week", date_labels =  "%b %d") +
+  geom_hline(yintercept=1)+
+  scale_fill_manual(values = c('grey','red'))+theme_bw()
+
+mvrCM<- mvr(Julian~., data = CM_PLS[,2:244],validation='CV',scale=TRUE,method="oscorespls")
+summary(mvrCM)
+mvrCMVIP<-VIP(mvrCM,opt.comp=3, p= 242)
+
+
+mvrVIP_CM_SMOOTH<-data.frame(
+  julian_day,mvrCMVIP)
+
+mvrVIP_CM_SMOOTH$realdate <- (seq(as.Date("1984/9/1"),
+                               as.Date("1985/4/30"), "days"))
+colnames(mvrVIP_CM_SMOOTH) <- c('julian','vip','realdate')
+mvrVIP_CM_SMOOTH$great <- 
+  ifelse(mvrVIP_CM_SMOOTH$vip >= 1, TRUE,FALSE)
+
+ggplot(mvrVIP_CM_SMOOTH, 
+       aes(x = realdate, y= vip, fill = great))+geom_bar(stat='identity',
+                                                         color='black')+
+  theme(axis.text.x = element_text(angle = 45, size = 9))+
+  theme(axis.ticks.length=unit(0.5,"cm"))+
+  scale_x_date(date_breaks = "2 week", date_labels =  "%b %d") +
+  geom_hline(yintercept=1)+
+  scale_fill_manual(values = c('grey','red'))+theme_bw()
+
+
+RC_CM_SMOOTH<-data.frame(julian_day,
+                         RC(mvrCM, opt.comp=1))
+
+RC_CM_SMOOTH$realdate <- (seq(as.Date("1984/9/1"),
+                               as.Date("1985/4/30"), "days"))
+
+colnames(RC_CM_SMOOTH)<- c('Julian','RC','Date')
+
+
+ggplot(RC_CM_SMOOTH, 
+  aes(x = Date, y= RC))+
+  geom_point()+geom_line()+
+  theme(axis.text.x = element_text(angle = 45, size = 9))+
+  scale_x_date(date_breaks = "2 week", date_labels =  "%b %d")+
+  geom_hline(yintercept =0)+
+  theme_bw()
 
--- Spring_Flight_Trend/spring_flight_figures.R
+++ Spring_Flight_Trend/spring_flight_figures.R
@@ -1,68 +1,108 @@
 ######################
 #SPRING FLIGHT GRAPHS#
 ######################
+#packages
+
+library(ggplot2)
+library(gridExtra)
+
 
 #1) THIS INCLUDES THE GRAPHING OF THE ANNUAL SPRING FLIGHT OF ALL FIVE SPECIES
 
 ###GENERAL CODE
-#theme_springflight <- 
-#  theme_bw()+
-#  theme(panel.grid.major = element_blank(),
-#        panel.grid.minor = element_blank(),
-#        axis.line.x = element_line(color="black"),
-#        axis.line.y = element_line(color="black"),
-#        plot.title=element_text( size=12, vjust=0),
-#        axis.text.x=element_text(size=12, angle=90,vjust=0.5, hjust=1),
-#       axis.text.y=element_text(size=12),
-#        axis.title.x=element_text(size=12, vjust=0),
-#        axis.title.y=element_text(size=12, vjust=0))
+theme_springflight <- 
+  theme_bw()+
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(),
+        axis.line.x = element_line(color="black"),
+        axis.line.y = element_line(color="black"),
+        plot.title=element_text( size=12, vjust=0),
+        axis.text.x=element_text(size=12, angle=90,vjust=0.5, hjust=1),
+       axis.text.y=element_text(size=12),
+        axis.title.x=element_text(size=12, vjust=0),
+       axis.title.y=element_text(size=12, vjust=0))
  
-#I think I like theme_publication() better
+#bitch to work with theme_publication()
 #---------------------------------------------------------------------------#
 
 
 #Codling moth 
 
-CM_FLIGHT <- ggplot(na.omit(CM.first.spring), aes(x= year, y= julian))+
-  geom_point(size=2)+geom_line()+theme_Publication()+
+CM_FLIGHT <- ggplot(CM.first.spring, aes(x= year, y= julian))+
+  geom_point(size=1.5)+geom_line()+theme_springflight+
   labs(x= "Year", y= "Julian day")+
-  labs(title = "Cydia pomonella")+scale_x_continuous(breaks=seq(1984,2016,4)); CM_FLIGHT
+  labs(title = "Cydia pomonella")+scale_x_continuous(breaks=seq(1984,2016,4),
+                                                    labels = seq(1984,2016,4))+
+  ylim(70,170); CM_FLIGHT
 #Oblique banded leafroller
 
 
-OB_FLIGHT<-ggplot(na.omit(ob.first.spring), aes(x= year, y= julian))+
-  geom_point(size=2)+geom_line()+theme_Publication()+
+OB_FLIGHT<-ggplot(ob.first.spring, aes(x= year, y= julian))+
+  geom_point(size=1.5)+geom_line()+theme_springflight+
   labs(x= "Year", y= "Julian day")+
-  labs(title = "Choristoneura rosaceana")+scale_x_continuous(breaks=seq(1981,2016,4)) 
+  labs(title = "Choristoneura rosaceana")+
+  scale_x_continuous(breaks =seq(1981,2016,by=5))+
+  ylim(70,170)
 OB_FLIGHT
 
-TA_FLIGHT <-  ggplot(na.omit(ta.first.spring), aes(x= year, y= julian))+
-  geom_point(size=2)+geom_line()+theme_Publication()+
+TA_FLIGHT <-  ggplot(ta.first.spring, aes(x= year, y= julian))+
+  geom_point(size=1.5)+geom_line()+theme_springflight+
   labs(x= "Year", y= "Julian day")+
-  labs(title = "Platynota idaeusalis")+scale_x_continuous(breaks=seq(1981,2016,4))
+  labs(title = "Platynota idaeusalis")+
+  scale_x_continuous(breaks =seq(1981,2016,by=5))+
+  ylim(70,170)
 TA_FLIGHT
 
+#--------------------------------------------------#
+#############################
+#Oriental fruit moth - Peach#
+#############################
 
-OFMP_FLIGHT<-ggplot(na.omit(ofm.first.spring_PCH), aes(x= year, y= julian ))+
-  geom_point(size=2)+geom_line()+ theme_Publication()+
-  labs(x= "Year", y= "Julian day")+
-  labs(title = "Oriental Fruit Moth- Peach")+scale_x_continuous(breaks=seq(1999,2016,2))
-OFMP_FLIGHT
+#OFMP_FLIGHT<-ggplot(na.omit(ofm.first.spring_PCH), aes(x= year, y= julian ))+
+ # geom_point(size=2)+geom_line()+theme_springflight+
+#  labs(x= "Year", y= "Julian day")+
+#  labs(title = "Oriental Fruit Moth- Peach")+scale_x_continuous(breaks=seq(1999,2016,2))
+#OFMP_FLIGHT
 
-OFMA_FLIGHT<-ggplot(na.omit(ofm.first.spring_APP), aes(x= year, y= julian ))+
-  geom_point(size=2)+geom_line()+ 
-  labs(x= "Year", y= "Julian day")+ theme_Publication()+
-  labs(title = "Oriental Fruit Moth- Apple")+scale_x_continuous(breaks=seq(1981,2016,4))
-OFMA_FLIGHT
+#OFMA_FLIGHT<-ggplot(na.omit(ofm.first.spring_APP), aes(x= year, y= julian ))+
+ # geom_point(size=2)+geom_line()+ 
+#  labs(x= "Year", y= "Julian day")+theme_springflight+
+#  labs(title = "Oriental Fruit Moth- Apple")+scale_x_continuous(breaks=seq(1981,2016,4))
+#OFMA_FLIGHT
 
+###AGGREGATED
+
+of_PCH <- ofm.first.spring_PCH[,c(3,5)]
+of_PCH$ id <- 'Peach'
+of_APP <- ofm.first.spring_APP[,c(3,5)]
+of_APP$id <- 'Apple'
+
+of_ALL <- rbind(of_PCH, of_APP)
+
+######################
+#Oriental fruit moth#
+#####################
+
+OFM_FLIGHT<-ggplot(of_ALL, aes(x= year, y= julian, group =id))+  
+  geom_point(size=1.5)+geom_line(aes(linetype=id))+theme_springflight+
+  labs(x= "Year", y= "Julian day")+labs(title = "Grapholita molesta")+
+    scale_x_continuous(breaks =seq(1981,2016,by=5))+
+  ylim(70,170)
 OFM_FLIGHT
 
-
-
-RB_FLIGHT <-ggplot(na.omit(rb.first.spring), aes(x= year, y= julian ))+
-  geom_point(size=2)+geom_line()+
-  labs(x= "Year", y= "Julian day")+ theme_Publication()+
-  labs(title = "Argyrotaenia velutina")+scale_x_continuous(breaks=seq(1981,2016,4))
+#######################
+#Red banded leafroller#
+#######################
+RB_FLIGHT <-ggplot(rb.first.spring, aes(x= year, y= julian ))+
+  geom_point(size=1.5)+geom_line()+
+  labs(x= "Year", y= "Julian day")+theme_springflight+
+  labs(title = "Argyrotaenia velutina")+scale_x_continuous(breaks=seq(1981,2016,5))+
+  ylim(70,170)
 RB_FLIGHT
 
-grid.arrange(CM_FLIGHT, OB_FLIGHT, TA_FLIGHT )
+grid.arrange(CM_FLIGHT, OB_FLIGHT, TA_FLIGHT,OFM_FLIGHT, RB_FLIGHT,ncol=3 )
+
+###ANOTHER WAY TO ARRANGE: BI VERSUS MULTI?
+
+###########################################
+#############################################
